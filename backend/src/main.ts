4bfc2ad937c5e9b55a8b2eba75e6d6328eb25b06
David Duranty <dadaduranty@gmail.com>
2025-06-23 12:10:27 +0200

    chore: redeploy backend on Render

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://carambar-h8vb.onrender.com',
+    origin: 'https://carambar-h8vb.onrender.com/',
   });
   await app.listen(process.env.PORT ?? 3000);
-  // Mise à jour pour forcer le déploiement Render
 
 }
 bootstrap();
