e83d8b0664e4487ecccb65f80babc47e1be31e15
David Duranty <dadaduranty@gmail.com>
2025-06-23 09:48:59 +0200

    chore: redeploy backend on Render

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
     origin: 'http://localhost:4200',
   });
   await app.listen(process.env.PORT ?? 3000);
+  // Mise à jour pour forcer le déploiement Render
 
 }
 bootstrap();
