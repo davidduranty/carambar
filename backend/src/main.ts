3a7beab23349fb20b403fb2712097b929e2367a4
David Duranty <dadaduranty@gmail.com>
2025-06-23 11:08:22 +0200

    fix: autorise frontend Render dans les CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: 'https://carambar-h8vb.onrender.com',
   });
   await app.listen(process.env.PORT ?? 3000);
   // Mise à jour pour forcer le déploiement Render
